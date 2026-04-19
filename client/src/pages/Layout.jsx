5f6cbdd3a339f92dcfde28589db6d209e2f9e8db
Yogesh01010 <yogeshsharm26879@gmail.com>
2026-04-19 11:26:09 +0530

    sidebar ui Completed

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
 
 
 const Layout = () => {
   return (
     <div className="flex h-screen bg-linear-to-br from-slate-50 via-white to-indigo-50/30">
-      <p>sidebar</p>
+      <Sidebar />
       <main className="flex-1 overflow-y-auto ">
         <div className="p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-800 mx-auto">
           <Outlet/>
@@ -14,4 +15,4 @@ const Layout = () => {
   )
 }
 
-export default Layout
+export default Layout;
